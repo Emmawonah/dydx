849e57234ee3c349060bdecfc764b119277e6076
Emmanuel Wonah <oewonah@gmail.com>
2023-08-17 22:55:21 +0100

    removed rate limit

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use((req, res, next) => {
 });
 
 // Rate Limiting
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
+//const limiter = rateLimit({
+    // windowMs: 15 * 60 * 1000, // 15 minutes
+    // max: 100, // Limit each IP to 100 requests per windowMs
+// });
+// app.use(limiter);
 
 // Routes
 const urlRoutes = require('./routes/url.route');
